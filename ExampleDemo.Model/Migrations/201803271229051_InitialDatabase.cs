a6279757fde98b2b5cf624fd8a37686e1d528bbe
S2WHYLOV3\s2whylov3 <nguyenhongcuong69@gmail.com>
2018-03-27 19:31:33 +0700

    Trong project Class library ExampleDemo.Model thực hiện Initial Database bằng Migrations

---
+++ ExampleDemo.Model/Migrations/201803271229051_InitialDatabase.cs
@@ -0,0 +1,48 @@
+namespace ExampleDemo.Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialDatabase : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Country",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 100),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Person",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        Phone = c.String(nullable: false, maxLength: 20),
+                        Address = c.String(nullable: false, maxLength: 100),
+                        State = c.String(nullable: false, maxLength: 50),
+                        CountryId = c.Int(nullable: false),
+                        CreatedDate = c.DateTime(nullable: false),
+                        CreatedBy = c.String(maxLength: 256),
+                        UpdatedDate = c.DateTime(nullable: false),
+                        UpdatedBy = c.String(maxLength: 256),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Country", t => t.CountryId, cascadeDelete: true)
+                .Index(t => t.CountryId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Person", "CountryId", "dbo.Country");
+            DropIndex("dbo.Person", new[] { "CountryId" });
+            DropTable("dbo.Person");
+            DropTable("dbo.Country");
+        }
+    }
+}
